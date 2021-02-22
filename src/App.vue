f0674b29ca44d29964fb38e4ee6d8aa31430a238
root <root@nms6.vir999.com>
2021-02-22 11:48:31 +0800

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <div class="row no-gutters">
+      <!-- 選擇地區 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="city" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+            <select name="" id="city" class="form-control">
+              <!-- 製作下拉選單 -->
+            </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="dist" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <select name="" id="dist" class="form-control">
+              <!-- 製作下拉選單 -->
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示地圖和 ubike 站點 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import 'bootstrap/scss/bootstrap';
+#map {
+  height: 100vh;
+  position: relative;
 }
 </style>
